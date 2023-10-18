5908d91247eb0139b42af5c64fcf0540ecf0ef35
Guddu Kumar <136989395+masterguddug@users.noreply.github.com>
2023-10-18 16:53:43 +0530

    Update send_sms.7.x.php

--- quickstart/php/sms/send_sms/send_sms.7.x.php
+++ quickstart/php/sms/send_sms/send_sms.7.x.php
@@ -1,23 +1,26 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-use Twilio\Rest\Client;
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import re
 
-// Your Account SID and Auth Token from twilio.com/console
-// To set up environmental variables, see http://twil.io/secure
-$account_sid = getenv('TWILIO_ACCOUNT_SID');
-$auth_token = getenv('TWILIO_AUTH_TOKEN');
-// In production, these should be environment variables. E.g.:
-// $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
+app = Flask(__name__)
 
-// A Twilio number you own with SMS capabilities
-$twilio_number = "+15017122661";
+@app.route('/whatsapp', methods=['POST'])
+def whatsapp_bot():
+    incoming_message = request.values.get('Body', '').lower()
 
-$client = new Client($account_sid, $auth_token);
-$client->messages->create(
-    // Where to send a text message (your cell phone?)
-    '+15558675310',
-    array(
-        'from' => $twilio_number,
-        'body' => 'I sent this message in under 10 minutes!'
-    )
-);
+    if contains_link(incoming_message):
+        response_message = "Warning: Please do not send links in this group."
+    else:
+        response_message = "Thank you for your message."
+
+    response = MessagingResponse()
+    response.message(response_message)
+
+    return str(response)
+
+def contains_link(text):
+    link_pattern = r'http[s]?://[^\s<>"]+|www\.[^\s<>"]+'
+    return bool(re.search(link_pattern, text))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
